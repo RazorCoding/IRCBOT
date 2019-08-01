152e06d5d302a12d73ac5db604e49cc456283bdf
Bushido <4154540+RazorCoding@users.noreply.github.com>
2019-08-01 23:55:39 +0530

    Update ideone_wKVSrD.cpp

--- ideone_wKVSrD.cpp
+++ ideone_wKVSrD.cpp
@@ -1,58 +1,82 @@
 #include<iostream>
 #include<sys/socket.h>
-#include<string>
 #include<sys/types.h>
-#include<cstring>
+#include<string.h>
+
+//#include<cstring>
 #include<netdb.h>
 #include<errno.h>
-
+#include<chrono>
 using namespace std;
-int main()
+int main(int argc, char *argv[])
 {
-	string nick = "NICK Bot1999292\r\n";
+	if(argc == 1)
+	{
+		std::cout << "IP-ADDRESS";
+		return 1;
+	}
+	//IRC Arguments
+	string nick = "NICK bot234\r\n";
         string user = "USER Bot Bot Bot :a bot\r\n";
-	std::string channel = "JOIN #spe\r\n";
-	std::string ping = "PONG :lindbohm.freenode.net\r\n";
-	int connected = 0;
+	std::string channel = "JOIN #Darkscience\r\n";
+	std::string ping = "PONG :\r\n";
+        std::string msg = " privmsg #darkscience : Hi guys...Sup?\r\n";	
 	
+	//Variables
+	int connected = 0;
 	struct sockaddr_in addr;
 	struct hostent *host;
 	int port = 6667;
-	host = gethostbyname("irc.freenode.net");
+	host = gethostbyname(argv[1]);
+	char str[] = "PINGr\r\n";
+	char str1[]= "Hi\r\n";
 
-
+	//assigning IP Address
 	addr.sin_addr.s_addr = *(unsigned long*)host->h_addr;
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons((unsigned short)port);
-
+	
+	//Creating socket
 	int sockfd;
- 	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
 	if(sockfd < 0)
 	{
 		perror("socket:");
 	}
-
-	int status;
+	
+	//Connecting socket
+	int status =0;
 	status = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
 	if(status < 0)
 	{
-		perror("connect:");
+		perror("connect");
 	}
 
-	cout << "Connecting to: irc.freenode.org" << endl;
+	 // Send
+	std::cout << "Connecting to: irc.freenode.org" << std::endl;
         send(sockfd, nick.c_str(), nick.size(), 0);
-        cout << "Sent: " << nick << " to server" << endl;
+	std::cout << "Sent: " << nick << " to server" << std::endl;
         send(sockfd, user.c_str(), user.size(), 0);
-        cout << "sent: " << user << " to server" << endl;
+	std::cout << "sent: " << user << " to server" << std::endl;
 	send(sockfd, channel.c_str(), channel.size(),0);
 	std::cout << "Channel:" << channel << " to server" << std::endl; 
-          char sockbuff[4096];
-        while (connected < 1) {
-		
-		send(sockfd,ping.c_str(),ping.size(),0);
+	
+
+	//Reacving
+	char sockbuff[4096]; 
+	while (connected < 1) {	
+                if(strcmp(str,sockbuff) != NULL )
+		{
+		   std::cout << "PONG:!" << std::endl;
+		   send(sockfd, ping.c_str(), ping.size(),0); 
+		}	
+		if(strcmp(str1,sockbuff) != NULL)
+		{
+			send(sockfd, msg.c_str(), msg.size(),0);
+		}
 		memset(&sockbuff, '\0', sizeof(sockbuff));
                 recv(sockfd, sockbuff, 4096, 0); 
-                cout << sockbuff << endl;
-        }
+		std::cout << sockbuff << std::endl;	
+ 	}
   return 0;
 }
