38996400d07a92c9f912668461bddb7a8d8b4831
Bushido <4154540+RazorCoding@users.noreply.github.com>
2019-08-02 21:48:26 +0530

    Update ideone_wKVSrD.cpp

--- ideone_wKVSrD.cpp
+++ ideone_wKVSrD.cpp
@@ -2,35 +2,34 @@
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<string.h>
-
-//#include<cstring>
+#include<cstring>
 #include<netdb.h>
 #include<errno.h>
 #include<chrono>
+
+
 using namespace std;
 int main(int argc, char *argv[])
 {
-	if(argc == 1)
-	{
-		std::cout << "IP-ADDRESS";
-		return 1;
-	}
+
 	//IRC Arguments
-	string nick = "NICK bot234\r\n";
+	string nick = "NICK bot23224\r\n";
         string user = "USER Bot Bot Bot :a bot\r\n";
-	std::string channel = "JOIN #Darkscience\r\n";
+	std::string channel = "JOIN #default\r\n";
 	std::string ping = "PONG :\r\n";
-        std::string msg = " privmsg #darkscience : Hi guys...Sup?\r\n";	
+        std::string msg = "privmsg #channel : Hi \r\n";	
 	
 	//Variables
 	int connected = 0;
 	struct sockaddr_in addr;
 	struct hostent *host;
 	int port = 6667;
-	host = gethostbyname(argv[1]);
-	char str[] = "PINGr\r\n";
-	char str1[]= "Hi\r\n";
+	char Hostname = "irc.freenode.net";
 
+	//gethostbyname
+	
+	host = gethostbyname(Hostname);
+	
 	//assigning IP Address
 	addr.sin_addr.s_addr = *(unsigned long*)host->h_addr;
 	addr.sin_family = AF_INET;
@@ -62,9 +61,11 @@ int main(int argc, char *argv[])
 	std::cout << "Channel:" << channel << " to server" << std::endl; 
 	
 
+	char str[] = "PINGr\r\n";
+	 char str1[]= "!Hi\r\n";
 	//Reacving
 	char sockbuff[4096]; 
-	while (connected < 1) {	
+	while (connected < 1) {
                 if(strcmp(str,sockbuff) != NULL )
 		{
 		   std::cout << "PONG:!" << std::endl;
@@ -74,8 +75,8 @@ int main(int argc, char *argv[])
 		{
 			send(sockfd, msg.c_str(), msg.size(),0);
 		}
-		memset(&sockbuff, '\0', sizeof(sockbuff));
-                recv(sockfd, sockbuff, 4096, 0); 
+		memset(&sockbuff, '\0', sizeof(sockbuff)); 
+		recv(sockfd,sockbuff,4096,0);
 		std::cout << sockbuff << std::endl;	
  	}
   return 0;
