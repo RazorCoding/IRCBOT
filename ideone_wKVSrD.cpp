583fc2d15fe1778f9029a318e588ae5d4bb34cf5
Bushido <4154540+RazorCoding@users.noreply.github.com>
2019-08-08 15:56:54 +0530

    Update ideone_wKVSrD.cpp

--- ideone_wKVSrD.cpp
+++ ideone_wKVSrD.cpp
@@ -5,10 +5,9 @@
 #include<cstring>
 #include<netdb.h>
 #include<errno.h>
-#include<chrono>
 #include<unistd.h>
-#include<fstream>
-
+#include<regex>
+#include<string>
 
 int main(int argc, char *argv[])
 {
@@ -17,25 +16,27 @@ int main(int argc, char *argv[])
 	std::string nick = "NICK bot23224\r\n";
 	std::string user = "USER Bot Bot Bot :a bot\r\n";
 	std::string channel = "JOIN #default\r\n";
-	std::string ping = "PONG \r\n";
-        std::string msg = "privmsg #default : Pong \r\n";	
-	
+	std::string ping = "PONG :";
+        std::string msg = "privmsg #default : Pong \r\n";
+
 	//Variables
 	int connected = 0;
 	struct sockaddr_in addr;
 	struct hostent *host;
 	int port = 6667;
 	char hostname[] = "irc.freenode.net";
-
+	SSL_CTX *sslctx;
+	SSL *cSSL;
 	//gethostbyname
-	
+
 	host = gethostbyname(hostname);
-	
+
 	//assigning IP Address
 	addr.sin_addr.s_addr = *(unsigned long*)host->h_addr;
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons((unsigned short)port);
-	
+
+
 	//Creating socket
 	int sockfd;
 	sockfd = socket(AF_INET,SOCK_STREAM,0);
@@ -43,7 +44,7 @@ int main(int argc, char *argv[])
 	{
 		perror("socket:");
 	}
-	
+
 	//Connecting socket
 	int status =0;
 	status = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
@@ -53,38 +54,41 @@ int main(int argc, char *argv[])
 	}
 
 	 // Send
-	std::cout << "Connecting to: irc.freenode.org" << std::endl;
+	std::cout << "Connecting to: "<< hostname << std::endl;
         send(sockfd, nick.c_str(), nick.size(), 0);
 	std::cout << "Sent: " << nick << " to server" << std::endl;
         send(sockfd, user.c_str(), user.size(), 0);
 	std::cout << "sent: " << user << " to server" << std::endl;
 	send(sockfd, channel.c_str(), channel.size(),0);
-	std::cout << "Channel:" << channel << " to server" << std::endl; 
-	
+	std::cout << "Channel:" << channel << " to server" << std::endl;
 
-	char str[] = "PONG";
-	 char str1[]= "!ping\r\n";
-	
 	//Reacving
-	char sockbuff[4096]; 
-	
+	char sockbuff[4096];
+	std::string buff = " ";
+	std::smatch mt;
+    	std::regex r ("PING\\s*:?(.*)");
+	std::cout << buff << std::endl;
+
 	while (connected < 1) {
-		
-		memset(&sockbuff, '\0', sizeof(sockbuff));
-                recv(sockfd,sockbuff,4096,0);
-  			
-		if(strcmp(str,sockbuff) != 0 )
-		{
+	memset(&sockbuff, '\0', sizeof(sockbuff));
+        recv(sockfd,sockbuff,4096,0);
+	buff = sockbuff;
 
-			send(sockfd, ping.c_str(), ping.size(),0); 
-		}
-		if(strcmp(str1,sockbuff) != 0)
-		{
-			send(sockfd, msg.c_str(), msg.size(),0);
-		}
-			
-		std::cout << sockbuff << std::endl;		    
+	std::cout << buff << std::endl;
+
+	   if (std::regex_search (buff, mt, r))
+	   {
+		   	std::smatch::iterator it = mt.begin()+1; // First match is entire s
+			ping.append(*it);
+			ping.append("\r\n");
+			std::cout << ping;
+			send(sockfd,ping.c_str(),ping.size(),0);
+	   }
+	   if(buff.find("!PING") != std::string::npos);
+	   {
+		send(sockfd,msg.c_str(),msg.size(),0);
+
+	   }
 	}
-
   return 0;
 }
