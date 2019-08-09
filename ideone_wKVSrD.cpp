b968cac3bd71bbf6a68313c8609db73a9423b81e
Bushido <4154540+RazorCoding@users.noreply.github.com>
2019-08-09 21:52:47 +0530

    Update ideone_wKVSrD.cpp

--- ideone_wKVSrD.cpp
+++ ideone_wKVSrD.cpp
@@ -8,10 +8,20 @@
 #include<unistd.h>
 #include<regex>
 #include<string>
+//#include <openssl/applink.c>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <signal.h>
 
-int main(int argc, char *argv[])
+
+int main()
 {
-
+	int newsockfd;
+	SSL_CTX *sslctx;
+	SSL *cSSL;
+	int ssl_err;
+	        
 	//IRC Arguments
 	std::string nick = "NICK bot23224\r\n";
 	std::string user = "USER Bot Bot Bot :a bot\r\n";
@@ -23,10 +33,10 @@ int main(int argc, char *argv[])
 	int connected = 0;
 	struct sockaddr_in addr;
 	struct hostent *host;
-	int port = 6667;
+	int port = 6697;
 	char hostname[] = "irc.freenode.net";
+	
 	//gethostbyname
-
 	host = gethostbyname(hostname);
 
 	//assigning IP Address
@@ -42,16 +52,38 @@ int main(int argc, char *argv[])
 	{
 		perror("socket:");
 	}
+	
 
 	//Connecting socket
 	int status =0;
 	status = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
 	if(status < 0)
 	{
-		perror("connect");
+		perror("connect:");
 	}
+	
+	sslctx = SSL_CTX_new( SSLv23_server_method());
+        SSL_CTX_set_options(sslctx, SSL_OP_SINGLE_DH_USE);
+        int use_cert = SSL_CTX_use_certificate_file(sslctx, "/serverCertificate.pem" , SSL_FILETYPE_PEM);
+        int use_prv = SSL_CTX_use_PrivateKey_file(sslctx, "/serverCertificate.pem", SSL_FILETYPE_PEM);
 
-	 // Send
+        cSSL = SSL_new(sslctx);
+        SSL_set_fd(cSSL, newsockfd );
+        //Here is the SSL connect  
+        ssl_err = SSL_connect(cSSL);
+        
+	//Error occurred,
+	switch (SSL_get_error(cSSL, ssl_err))
+	{
+		case 1:
+			std::cout << ssl_err;
+		case 2:
+			std::cout << ssl_err;
+		case 3:
+			std::cout << ssl_err;
+	}
+	
+	// sending
 	std::cout << "Connecting to: "<< hostname << std::endl;
         send(sockfd, nick.c_str(), nick.size(), 0);
 	std::cout << "Sent: " << nick << " to server" << std::endl;
@@ -65,15 +97,11 @@ int main(int argc, char *argv[])
 	std::string buff = " ";
 	std::smatch mt;
     	std::regex r ("PING\\s*:?(.*)");
-	std::cout << buff << std::endl;
+	//std::regex x (!.*[A-Z]);
 
 	while (connected < 1) {
 	memset(&sockbuff, '\0', sizeof(sockbuff));
-        recv(sockfd,sockbuff,4096,0);
-	buff = sockbuff;
-
-	std::cout << buff << std::endl;
-
+	
 	   if (std::regex_search (buff, mt, r))
 	   {
 		   	std::smatch::iterator it = mt.begin()+1; // First match is entire s
@@ -82,11 +110,13 @@ int main(int argc, char *argv[])
 			std::cout << ping;
 			send(sockfd,ping.c_str(),ping.size(),0);
 	   }
-	   if(buff.find("!PING") != std::string::npos);
+/*	   if(std::regex_search(buff , mt,x))
 	   {
-		send(sockfd,msg.c_str(),msg.size(),0);
-
+		std::cout << "!PONG" << std::endl;
 	   }
+	   recv(sockfd,sockbuff,4096,0);
+	   buff = sockbuff;
+	   std::cout << buff << std::endl;*/
 	}
   return 0;
 }
